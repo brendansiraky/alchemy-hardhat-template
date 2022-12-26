74c76b50b05c468d6d61d81bf358935989d35c21
Brendan Hemat-Siraky <brendansirakydeveloper@gmail.com>
2022-12-26 14:28:55 +1000

    prettiered it

--- helpers/getBalance.js
+++ helpers/getBalance.js
@@ -3,10 +3,7 @@ const { Utils } = require('alchemy-sdk')
 
 async function getBalance(address) {
     try {
-        const unformattedBalance = await provider.getBalance(
-            address,
-            'latest'
-        )
+        const unformattedBalance = await provider.getBalance(address, 'latest')
         const formattedBalance = Utils.formatEther(unformattedBalance)
         console.log(formattedBalance)
         return formattedBalance
--- scripts/interact.js
+++ scripts/interact.js
@@ -10,14 +10,12 @@ const getBalance = require('../helpers/getBalance')
 const CONTRACT_ADDRESS = '0x4EE92526865462207133B1c35D7E10df116DC6a9'
 
 async function interact() {
-    const contract = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        abi,
-        signer,
-    )
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer)
     const message = await contract.message()
     const balance = await getBalance(CONTRACT_ADDRESS)
-    console.log(`The message is ${message} and the balance in ether is ${balance}`)
+    console.log(
+        `The message is ${message} and the balance in ether is ${balance}`
+    )
 }
 
 interact()
